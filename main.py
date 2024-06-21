e9d15b4f04b8e4ccb07f599406821652c280e10c
tferracina <91603274+tferracina@users.noreply.github.com>
2024-06-22 01:31:15 +0200

    incorporated downloaded model
    now we can use our downloaded model in the pipeline

--- main.py
+++ main.py
@@ -5,8 +5,15 @@ import os
 import glob
 import json
 from huggingface_hub import hf_hub_download
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
+from dotenv import load_dotenv
 
-HUGGINGFACE_API_TOKEN = os.getenv('HUGGINGFACE_API_TOKEN')
+
+#Set the environment variables
+load_dotenv()
+HUGGINGFACE_ACCESS_TOKEN = os.getenv('HUGGINGFACE_ACCESS_TOKEN')
+DIRECTORY_PATH = os.getenv('DIRECTORY_PATH')
+
 
 #Load the model
 model_id = 'knowledgator/comprehend_it-base'
@@ -24,12 +31,18 @@ for filename in filenames:
     downloaded_model_path = hf_hub_download(
         repo_id=model_id,
         filename=filename,
-        token=HUGGINGFACE_API_TOKEN
+        token=HUGGINGFACE_ACCESS_TOKEN
     )
+#print(f"Model downloaded to {downloaded_model_path}")
 
-print(f"Model downloaded to {downloaded_model_path}")
+#Create the classifier pipeline
+tokenizer = AutoTokenizer.from_pretrained(model_id, legacy=False)
+model = AutoModelForSequenceClassification.from_pretrained(model_id)
+
+pipeline = pipeline('zero-shot-classification', model=model, device=-1, tokenizer=tokenizer, max_length=512, truncation=True)
 
 
+#Define helper functions
 def rename_file(original_path, new_name):
     """
     Rename the file to the new name.
@@ -38,8 +51,7 @@ def rename_file(original_path, new_name):
     new_path = os.path.join(directory, new_name)
     os.rename(original_path, new_path)
     return new_path
- 
- 
+
 def ocr_core(filename):
     """
     Perform OCR on a single image, return the text
@@ -51,13 +63,12 @@ def ocr_core(filename):
     except Exception as e:
         print(f"Error processing {filename}: {e}")
         return None
- 
- 
+
 def perform_ocr(directory_path, output_file):
     """
     Perform OCR on all images in a given directory, save the results to a JSON file
     """
-    image_extensions = ['*.png', '*.jpg', '*.jpeg', '*.tiff', '*.bmp', '*.gif']
+    image_extensions = ['*.png', '*.jpg', '*.jpeg']
     image_paths = []
     for extension in image_extensions:
         image_paths.extend(glob.glob(os.path.join(directory_path, extension)))
@@ -80,36 +91,34 @@ def perform_ocr(directory_path, output_file):
     with open(output_file, 'w') as f:
         json.dump(image_data, f, indent=4)
  
- 
-directory_path = '/Users/tommasoferracina/alfagomma/file-reformatter/media'
 output_file = 'ocr_results.json'
-perform_ocr(directory_path, output_file)
- 
+perform_ocr(DIRECTORY_PATH, output_file)
+
  
 ## LLM
- 
-from transformers import pipeline
  
 def classify_texts(ocr_results):
     """
    Classify the OCR results with zero-shot classification, then change the file names accordingly
    """
-    classifier = pipeline('zero-shot-classification', model='knowledgator/comprehend_it-base', framework='pt')
  
     with open(ocr_results, 'r') as f:
         image_data = json.load(f)
  
     for entry in image_data:
         text = entry['ocr_text']
-        image_path = os.path.join(directory_path, entry['filename'])
+        image_path = os.path.join(DIRECTORY_PATH, entry['filename'])
  
         country_labels = ["australia", "uk", "singapore","malaysia", "southafrica"]
         layout_labels = ["freight", "utility", "product", "service"]
  
-        country = classifier(text, candidate_labels=country_labels)['labels'][0]
-        layout = classifier(text, candidate_labels=layout_labels)['labels'][0]
+        country = pipeline(text, candidate_labels=country_labels)['labels'][0]
+        layout = pipeline(text, candidate_labels=layout_labels)['labels'][0]
  
-        new_name = f"invoice-{country}-{layout}{os.path.splitext(image_path)[1]}"
-        rename_file(image_path, new_name)
+        #new_name = f"invoice-{country}-{layout}{os.path.splitext(image_path)[1]}"
+        #rename_file(image_path, new_name)
+        print(f"Classified {image_path} as {country} and {layout}")
  
-        return None
+    return None
+    
+classify_texts(output_file)
